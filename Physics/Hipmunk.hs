6390285811bdd91f764fa55423e26a768fe49b3c
Felipe A. Lessa <felipe.lessa@gmail.com>
2008-07-22 20:00:07 +0300

    New catch-all module.
    darcs-hash:20080722170007-e5958-16408b04f3b9bc64b1115474e9920dc6f3b50bb0

---
+++ Physics/Hipmunk.hs
@@ -0,0 +1,14 @@
+module Physics.Hipmunk
+    (module Physics.Hipmunk.Common,
+     module Physics.Hipmunk.Body,
+     module Physics.Hipmunk.Shape,
+     module Physics.Hipmunk.Joint,
+     module Physics.Hipmunk.Space
+    )
+    where
+
+import Physics.Hipmunk.Common
+import Physics.Hipmunk.Body
+import Physics.Hipmunk.Shape
+import Physics.Hipmunk.Joint
+import Physics.Hipmunk.Space
