4532b18a83c38897d04745903cb07e75202efae4
Felipe A. Lessa <felipe.lessa@gmail.com>
2009-05-05 15:27:54 +0300

    Update to Chipmunk 4.1.0.
    Ignore-this: d384e6d9a833be0c40cd53191019e611
    
    darcs-hash:20090505122754-e5958-5da3099721d98766630e4ba148b6b5ae8a3a0189

--- chipmunk/chipmunk.h
+++ chipmunk/chipmunk.h
@@ -19,11 +19,14 @@
  * SOFTWARE.
  */
 
+#ifndef CHIPMUNK_HEADER
+#define CHIPMUNK_HEADER
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 	
-typedef float cpFloat;
+typedef double cpFloat;
 	
 static inline cpFloat
 cpfmax(cpFloat a, cpFloat b)
@@ -83,4 +86,6 @@ cpFloat cpMomentForPoly(cpFloat m, int numVerts, cpVect *verts, cpVect offset);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif
--- chipmunk/cpBody.c
+++ chipmunk/cpBody.c
@@ -1,15 +1,15 @@
 /* Copyright (c) 2007 Scott Lembcke
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,7 +18,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+ 
 #include <stdlib.h>
 #include <math.h>
 #include <float.h>
@@ -36,21 +36,21 @@ cpBodyInit(cpBody *body, cpFloat m, cpFloat i)
 {
 	body->velocity_func = cpBodyUpdateVelocity;
 	body->position_func = cpBodyUpdatePosition;
-
+	
 	cpBodySetMass(body, m);
 	cpBodySetMoment(body, i);
 
 	body->p = cpvzero;
 	body->v = cpvzero;
 	body->f = cpvzero;
-
+	
 	cpBodySetAngle(body, 0.0f);
 	body->w = 0.0f;
 	body->t = 0.0f;
-
+	
 	body->v_bias = cpvzero;
 	body->w_bias = 0.0f;
-
+	
 	body->data = NULL;
 //	body->active = 1;
 
@@ -112,7 +112,7 @@ cpBodyUpdatePosition(cpBody *body, cpFloat dt)
 {
 	body->p = cpvadd(body->p, cpvmult(cpvadd(body->v, body->v_bias), dt));
 	cpBodySetAngle(body, body->a + (body->w + body->w_bias)*dt);
-
+	
 	body->v_bias = cpvzero;
 	body->w_bias = 0.0f;
 }
@@ -137,22 +137,22 @@ cpDampedSpring(cpBody *a, cpBody *b, cpVect anchr1, cpVect anchr2, cpFloat rlen,
 	// Calculate the world space anchor coordinates.
 	cpVect r1 = cpvrotate(anchr1, a->rot);
 	cpVect r2 = cpvrotate(anchr2, b->rot);
-
+	
 	cpVect delta = cpvsub(cpvadd(b->p, r2), cpvadd(a->p, r1));
 	cpFloat dist = cpvlength(delta);
 	cpVect n = dist ? cpvmult(delta, 1.0f/dist) : cpvzero;
-
+	
 	cpFloat f_spring = (dist - rlen)*k;
 
 	// Calculate the world relative velocities of the anchor points.
 	cpVect v1 = cpvadd(a->v, cpvmult(cpvperp(r1), a->w));
 	cpVect v2 = cpvadd(b->v, cpvmult(cpvperp(r2), b->w));
-
+	
 	// Calculate the damping force.
 	// This really should be in the impulse solver and can produce problems when using large damping values.
 	cpFloat vrn = cpvdot(cpvsub(v2, v1), n);
 	cpFloat f_damp = vrn*cpfmin(dmp, 1.0f/(dt*(a->m_inv + b->m_inv)));
-
+	
 	// Apply!
 	cpVect f = cpvmult(n, f_spring + f_damp);
 	cpBodyApplyForce(a, f, r1);
@@ -164,7 +164,7 @@ cpDampedSpring(cpBody *a, cpBody *b, cpVect anchr1, cpVect anchr2, cpFloat rlen,
 //{
 //	cpFloat ke = body->m*cpvdot(body->v, body->v);
 //	cpFloat re = body->i*body->w*body->w;
-//
+//	
 //	if(ke + re > body->m*dvsq)
 //		body->active = 1;
 //	else if(body->active)
@@ -175,6 +175,6 @@ cpDampedSpring(cpBody *a, cpBody *b, cpVect anchr1, cpVect anchr2, cpFloat rlen,
 //		body->w = 0.0f;
 //		body->w_bias = 0.0f;
 //	}
-//
+//	
 //	return body->active;
 //}
